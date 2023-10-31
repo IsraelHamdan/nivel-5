97c143ef602a98e7ccfb4e6e0ab83298e0f1309b
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-10-31 15:02:31 -0300

    rotas feitas e inicio da configuração do CORS

---
+++ React/server/livro-servidor/routes/livros.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const livroDados = require("../models/livro-dados");
+
+router.get("/", async (req, res) => {
+  try {
+    const livros = await livroDados.obterLivros();
+    res.json(livros);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+router.post("/cadastro", async (req, res) => {
+  try {
+    const novoLivro = req.body;
+    const livroSalvo = await livroDados.incluir(novoLivro);
+    res.json(livroSalvo);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+router.delete("/:codigo", async (req, res) => {
+  const codigo = req.params.codigo;
+  try {
+    const resultado = await livroDados.excluir(codigo);
+    res.json({ message: resultado });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+module.exports = router;
