dffa019f15ad1cac3369ba20501c03eed4bab92d
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-11-14 14:37:31 -0300

    fixing API

--- React/server/livro-servidor/models/Editora.js
+++
@@ -1,8 +0,0 @@
-class Editora {
-  constructor(codEditora, nome) {
-    this.codEditora = codEditora;
-    this.nome = nome;
-  }
-}
-
-export default Editora;
--- React/server/livro-servidor/models/Livro.js
+++
@@ -1,9 +0,0 @@
-export default class Livro {
-  constructor(codigo, codEditora, titulo, resumo, autores) {
-    this.codigo = codigo;
-    this.codEditora = codEditora;
-    this.titulo = titulo;
-    this.resumo = resumo;
-    this.autores = autores;
-  }
-}
